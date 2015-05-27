6c0a44f3b8507b6a951077b0a93fa2af1344ccf6
georgelynch <georgelynch1980@hotmail.com>
2015-05-27 18:47:08 +0800

    mogolab fix

--- config/connections.js
+++ config/connections.js
@@ -33,8 +33,7 @@ module.exports.connections = {
   },
   heroku_mongolab : {
      adapter   : "sails-mongo",
-     url       : "mongodb://heroku_app37237626:georgelynch1980@ds041032.mongolab.com:41032/heroku_app37237626",
-     database:   "heroku_app36381899"
+     url       : "mongodb://heroku_app37237626:georgelynch1980@ds041032.mongolab.com:41032/heroku_app37237626",//mongodb://USER:PASSWORD@HOST:PORT/DB
   },  
   /***************************************************************************
   *                                                                          *
