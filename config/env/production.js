bea333b804ce057bd87e66381c7b23cc2348cef3
georgelynch <georgelynch1980@hotmail.com>
2015-06-12 17:18:12 +0800

    dfdfdfdfdfdfd

--- config/env/production.js
+++ config/env/production.js
@@ -16,28 +16,10 @@ module.exports = {
      * Set the default database connection for models in the production        *
      * environment (see config/connections.js and config/models.js )           *
      ***************************************************************************/
-    /*
+    
     models: {
         connection: 'heroku_mongolab'
     },
-    */
-    connections: {
-
-        heroku_mongolab: {
-            adapter: 'sails-mongo',
-            //url:process.env.MONGOLAB_URI
-            host: 'ds031912.mongolab.com',
-            port: 31912,
-            schema: 'true',
-            user: 'heroku_874lbclz',
-            password: '86po8r87dvgbm2a5grdr725dgf',
-            database: 'heroku_874lbclz'
-        }
-    },
-    models: {
-        -connection: 'heroku_mongolab'
-    },
-    port: 3000,
 
     /***************************************************************************
      * Set the port in the production environment to 80                        *
@@ -52,6 +34,6 @@ module.exports = {
     // log: {
     //   level: "silent"
     // }
-
+    
 
 };
