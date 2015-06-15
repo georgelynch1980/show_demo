b2e4a8222ceba8c15f090a7ee638505e477cf858
georgelynch <georgelynch1980@hotmail.com>
2015-06-15 14:45:48 +0800

    ggggg

--- config/env/development.js
+++ config/env/development.js
@@ -18,7 +18,7 @@ module.exports = {
      ***************************************************************************/
 
     models: {
-        connection: 'heroku_mongolab_url'
+        connection: 'localDiskDb'
     }
 
 
